fe41cb4503452a5ff62472d5c52b1b0ad29a736a
wataru <watari53@gmail.com>
2014-08-24 12:32:46 +0900

    Finish user edit, update, index, and destroy actions

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -76,10 +76,10 @@ describe "Authentication" do
 					before { patch user_path(user) }
 					specify { expect(response).to redirect_to(signin_path) }
 				end
-
+				
 				describe "visiting the user index" do
-					before { visit users_path}
-					it { should have_title('Sign in')}
+					before { visit users_path }
+					it { should have_title('Sign in') }
 				end
 			end
 		end
@@ -97,6 +97,18 @@ describe "Authentication" do
 
 			describe "submitting a PATCH request to the Users#update action" do
 				before { patch user_path(wrong_user) }
+				specify { expect(response).to redirect_to(root_path) }
+			end
+		end
+
+		describe "as non-admin user" do
+			let(:user) { FactoryGirl.create(:user) }
+			let(:non_admin) { FactoryGirl.create(:user) }
+
+			before { sign_in non_admin, no_capybara: true }
+
+			describe "submitting a DELETE request to the Users#destroy action" do
+				before { delete user_path(user) }
 				specify { expect(response).to redirect_to(root_path) }
 			end
 		end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,9 +5,10 @@ describe "User pages" do
   subject { page }
 
   describe "index" do
+
     let(:user) { FactoryGirl.create(:user) }
+
     before do
-      sign_in FactoryGirl.create(:user)
       sign_in user
       visit users_path
     end
@@ -20,13 +21,34 @@ describe "User pages" do
       before(:all) { 30.times { FactoryGirl.create(:user) } }
       after(:all) { User.delete_all }
 
-      it { should have_seletor('div.pagination') }
+      it { should have_selector('div.pagination') }
 
 
       it "should list each user" do
         User.paginate(page: 1).each do |user|
           expect(page).to have_selector('li', text: user.name)
         end
+      end
+    end
+
+    describe "delete links" do
+
+      it { should_not have_link('delete') }
+
+      describe "as an admin user" do
+        let(:admin) { FactoryGirl.create(:admin) }
+        before do
+          sign_in admin
+          visit users_path
+        end
+
+        it { should have_link('delete', href: user_path(User.first)) }
+        it "should be able to delete another user" do
+          expect do
+            click_link('delete', match: :first)
+          end.to change(User, :count).by(-1)
+        end
+        it { should_not have_link('delete', href: user_path(admin)) }
       end
     end
   end
