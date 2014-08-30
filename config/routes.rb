4951543e019ffbfc55e1e9d34d93958f0da88d37
watari53 <watari53@gmail.com>
2014-08-30 18:56:37 +0900

    complete tutorial

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,6 @@ SampleApp::Application.routes.draw do
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
 
-  match '/spot',    to: 'spots#top',    via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
